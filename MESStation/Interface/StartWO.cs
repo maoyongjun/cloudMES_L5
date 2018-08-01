c29067e907adb18360a34fcf4408965b4362ca69
maoyongjun <H7108464@foxconn.com>
2018-08-01 16:59:21 +0800

    展工单的规则

---
+++ MESStation/Interface/StartWO.cs
@@ -0,0 +1,44 @@
+﻿using MESDataObject;
+using MESDBHelper;
+using MESStation.BaseClass;
+using MESStation.SNMaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MESStation.Interface
+{
+    class StartWO : MesAPIBase
+    {
+        protected APIInfo info = new APIInfo()
+        {
+            FunctionName = "startWO",
+            Description = "start WO For TjL5",
+            Parameters = new List<APIInputInfo>()
+            {
+                new APIInputInfo() {InputName = "WO", InputType = "STRING", DefaultValue = "WO"}
+            },
+            Permissions = new List<MESPermission>()//不需要任何權限
+        };
+        public StartWO()
+        {
+            this.Apis.Add(info.FunctionName, info);
+        }
+
+        public void startWO(Newtonsoft.Json.Linq.JObject requestValue, Newtonsoft.Json.Linq.JObject Data, MESStationReturn StationReturn)
+        {
+            OleExec Sfcdb = this.DBPools["SFCDB"].Borrow();
+            string WO = Data["WO"].ToString();
+
+            String nextSN = SNmaker.GetNextSN("TEST", Sfcdb);
+
+            StationReturn.Data = nextSN;
+            StationReturn.Status = StationReturnStatusValue.Pass;
+            StationReturn.Message = MESReturnMessage.GetMESReturnMessage("MSGCODE20180801141046");
+
+        }
+
+    }
+}
