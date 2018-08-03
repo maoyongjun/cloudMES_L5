cffd88e0c39a5e2546d8aa53be515908b720257c
maoyongjun <H7108464@foxconn.com>
2018-08-03 09:35:10 +0800

    增加展开的SN的写入

--- MESDataObject/Module/R_SN.cs
+++ MESDataObject/Module/R_SN.cs
@@ -1114,6 +1114,24 @@ namespace MESDataObject.Module
             }
             return TRCode;
         }
+
+
+
+        public string addStartSNRecords(string workorderno, string trsn, OleExec DB) {
+            T_R_SN SnDetailTable = new T_R_SN(DB, this.DBType);
+            Row_R_SN r_sn = (Row_R_SN)SnDetailTable.NewRow();
+            string result = string.Empty;
+            r_sn.ID = SnDetailTable.GetNewID("TJ", DB);
+            r_sn.SN = trsn;
+            r_sn.EDIT_TIME = DateTime.Now;
+            r_sn.WORKORDERNO = workorderno;
+            string sql = r_sn.GetInsertString(this.DBType);
+            result = DB.ExecSQL(sql);
+
+            return result;
+        }
+
+
         //Add by LLF 2018-01-26 End
 
         /// <summary>
--- MESStation/Interface/StartWO.cs
+++ MESStation/Interface/StartWO.cs
@@ -35,10 +35,12 @@ namespace MESStation.Interface
             T_R_WO_HEADER_TJ t_R_WO_HEADER_TJ = new T_R_WO_HEADER_TJ(Sfcdb,this.DBTYPE);
             Row_R_WO_HEADER_TJ row_R_WO_HEADER = t_R_WO_HEADER_TJ.GetWo(WO,Sfcdb);
             float qty = float.Parse(row_R_WO_HEADER.GAMNG);
-          
+            qty = 5;
             for (int i = 0; i < qty; i++) {
                 String nextSN = SNmaker.GetNextSN("TEST", Sfcdb, WO);
                 Console.Out.WriteLine(nextSN);
+                T_R_SN t_r_sn = new T_R_SN(Sfcdb,this.DBTYPE);
+                t_r_sn.addStartSNRecords(WO, nextSN, Sfcdb);
             }
             
 
